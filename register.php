e792a54511846cb9c3f872122ccc9b23bbafdb45
hashika Maduranga <m.g.hashikamaduranga@gmail.com>
2019-07-08 18:35:07 +0530

    modified register file

--- register.php
+++ register.php
@@ -10,7 +10,7 @@ $dbpassword="";
 $dbname="test";
 
 $conn=new mysqli($host,$dbname1,$dbpassword,$dbname);
-//$conn=mysqli_connect('localhost','root','','test');
+
 if(mysqli_connect_error()){
     die('connect error('.mysqli_connect_errno().') '.mysqli_connect_error());
 }
@@ -20,6 +20,7 @@ else{
     if($conn->query($sql)===TRUE){
      /* $message= "new record inserted succsesful";
           echo "<script type='text/javascript'>alert('$message');</script>";*/
+          header('Locatin:shoppingnew.php');
     }
     else{
         echo "error: ".$sql."<br>".$conn->error;
@@ -33,56 +34,95 @@ else{
 <head>
 
     <title><img src="Ca.PNG " height="60px" width="150px">Registration login and logout</title>
+    <link href="style.css">
     <style>
-    .header{
-        background-color:brown;
-        height:80px;
-        margin:20px;
-        line-height: 35px;
-        text-align: center;
-        line-height:60px;
-
-    }
-    .frm{
-        border:solid black 5px;
-        width:650px;
-      
-        border-radius:10px;
-        margin:150px auto;
-        background:#1456;
-        height:200px;
-
-
-    }
-    .frm tr{
-       background-color:black;
-       margin:auto;
-    }
-    .frm tr td label{
-      font-size: 10px;
-      color:white;
-      padding:5px;
-    }
-
-    .textinput{
-        color:black;
-        background:#3345;
-        padding:5px;
-        margin-left:70%;
-        text-align: left;
-    }
     body{
-        background-image:url("img2.jpg");
-        width:100%;
-        height:100%;
-        position:fixed;
-        content:'';
-        background-position:center center;
-        background-repeat:no-repeat;
-        background-attachment:fixed;
-        background-size:cover;
-    }
+	margin:0;
+	padding:0;
+	background-image:url(bg2.jpg);
+	width: 100%;
 
+	background-size: cover;
+	
+	font-family: sans-serif;
+}
+.header h1{
+    font-size:50px;
+    margin-top:50px;
+    background-color:brown;
+}
+.loginbox{
+	width: 320px;
+	height: 500px;
+	background:#000;
+	color:#fff;
+	top: 0%;
+	left: 50%;
+	position: absolute;
+	transform: translate(-50%,50%);
+	box-sizing: border-box;
+	padding: 70px 30px
+}
+.avatar{
+	width: 100px;
+	height: 100px;
+	border-radius: 50%;
+	position: absolute;
+	top: -50px;
+	left: calc(50%-50px); 
+
+}
+h1{
+	margin:0;
+	padding:0 0 20px;
+	text-align:center;
+	font-size: 22px;
+}
+.loginbox p{
+	margin:0;
+	padding: 0;
+	font-weight: bold;
+}
+
+.loginbox input{
+	width: 100%;
+	margin-bottom: 20px;
+
+}
+.loginbox input[ type="text"],input[type="password"]{
+	border: none;
+	border-bottom: 1px solid #fff;
+	background: transparent;
+	outline: none;
+	height: 40px;
+	color: #fff;
+	font-size: 16px
+
+}
+.loginbox input[type="submit"]{
+
+	border:none;
+	outline: none;
+	height: 40px;
+	background:#fb2525;
+	color:#fff;
+	font-size: 18px;
+	border-radius: 20px;
+}
+.loginbox input[type="submit"]:hover{
+	cursor: pointer;
+	background:#ffc107;
+	color: #000;
+}
+.loginbox a{
+	text-decoration: none;
+	font-size:12px;
+	line-height: 20px;
+	color:darkgrey;
+}
+.loginbox a:hover{
+	color:#ffc107;
+}
        </style>
 </head>
 <body>
@@ -90,30 +130,22 @@ else{
     <div class ="header">
     <h1>Registration <a href="shoppingnew.php"><span>Hash</span>Lanka</a></h1>
 </div>
-<form method="post" action="register.php">
-   <div class="frm">
-        <tr>
-            <td ><label>Username</label></td>
-            <td> <input type="text" name="username" class="textinput"></td>
-        </tr>
-        <tr>
-            <td ><label>Email</label></td>
-            <td> <input type="email" name="email" class="textinput"></td>
-        </tr>
-        <tr>
-            <td><label>Password</label></td>
-            <td> <input type="password" name="password" class="textinput"></td>
-        </tr>
-       <!-- <tr>
-           <td>Confirm Password</td>
-            <td> <input type="password" name="password2" class="textinput"></td>
-        </tr>-->
-        <tr>
-            
-            <td> <input type="submit" name="register_btn" value="register"><button><a href="sign_in.php">logout</a></button></td>
-        </tr>
-</div>
-</form>
+<div class="loginbox">
+		
+		<h1>Login Here</h1>
+		<form>
+			<p>username</p>
+			<input type="text" name="username" placeholder="username">
+      <p>email</p>
+      <input type="text" name="email" placeholder="email">
+     
+			<p>password</p>
+			<input type="password" name="password" placeholder="password">
+			<input type="submit" name="" value="Login">
+			<a href="">Lost your password?</a><br>
+			<a href=""> Don't have an account?</a>
+		</form>
+	</div>
 
 </body>
 </html>
